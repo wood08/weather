207bf45ffb5c2c52c2f9314817453fe80e511a7b
wood08 <wood08@hotmail.com>
2019-06-12 21:50:07 +0900

    로딩 화면 추가

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+    state = {
+        isLoaded: false
+    };
+    render() {
+        const {isLoaded} = this.state;
+        return (
+            <View style={styles.container}>
+                {isLoaded ? null:
+                    (<View style={styles.loading}>
+                        <Text style={styles.loadingText}>Getting the Weather</Text>
+                    </View>)
+                }
+            </View>
+        );
+    }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        flexDirection: 'row'
+    },
+    loading: {
+        flex: 1,
+        backgroundColor: "#FDF6AA",
+        justifyContent: "flex-end",
+        paddingLeft: 30
+    },
+    loadingText: {
+        fontSize: 40,
+        marginBottom: 100
+    }
+
 });
